04d14c876bb724a81a4e31364d90b1a18216ac04
Mica <jongeau-m@hotmail.fr>
2025-04-14 09:51:12 +0200

    Mise en place de la fonction login utilisateur avec redirection vers index.html et message en cas d'erreur.

---
+++ FrontEnd/scripts/login.js
@@ -0,0 +1,45 @@
+function loginUtilisateur() {
+    const loginForm = document.querySelector("#login form");
+    const inputEmail = document.getElementById("email");
+    const inputMdp = document.getElementById("mot-de-passe");
+    const errorMsg = document.getElementById("error-message");
+
+    loginForm.addEventListener("submit", async (event) => {
+        event.preventDefault();
+
+        const emailValue = inputEmail.value.trim();
+        const mdpValue = inputMdp.value.trim();
+
+        if (emailValue === "" || mdpValue === "") {
+            errorMsg.innerText = "Veuillez remplir tous les champs";
+            return;
+        }
+
+        const chargeUtile = {
+            email: emailValue,
+            password: mdpValue,
+        };
+
+        try {
+            const reponse = await fetch("http://localhost:5678/api/users/login", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(chargeUtile),
+            });
+
+            const data = await reponse.json();
+
+            if (reponse.ok) {
+                localStorage.setItem("token", data.token);
+                window.location.href = "index.html";
+            } else {
+                errorMsg.innerText = "Erreur dans l'identifiant ou le mot de passe";
+            }
+        } catch (error) {
+            console.error("Erreur lors de la connexion :", error);
+            errorMsg.innerText = "Une erreur est survenue, réessayez plus tard";
+        }
+    });
+}
+
+loginUtilisateur();
--- FrontEnd/scripts/works.js
+++ FrontEnd/scripts/works.js
@@ -1,3 +1,4 @@
+// Fonction d'appel à l'API pour récuperer les works et les categories :
 async function appelApi(worksListe, filtresListe) {
     const worksReponse = await fetch("http://localhost:5678/api/works");
     worksListe = await worksReponse.json();
@@ -5,14 +6,16 @@ async function appelApi(worksListe, filtresListe) {
     const categoriesReponse = await fetch("http://localhost:5678/api/categories");
     filtresListe = await categoriesReponse.json();
 
+    // Retourne la liste des projets (works) et la liste des catégories (filtres) :
     return {worksListe, filtresListe};
 }
 
 
+// Fonction qui ajoute les figures des works dans le DOM de façon dynamique :
 async function affichageWorks(worksListe) {
 
     const workGallery = document.querySelector(".gallery");
-    workGallery.innerHTML = ""; // on vide la galerie avant de l'afficher à nouveau
+    workGallery.innerHTML = ""; // On vide la galerie avant de l'afficher à nouveau
 
     for(let i = 0; i < worksListe.length; i++) {
         const work = worksListe[i];
@@ -33,20 +36,28 @@ async function affichageWorks(worksListe) {
 }
 
 
+// Fonction d'affichage des boutons filtres ET filtrage :
 async function affichageFiltres() {
 
     const {worksListe, filtresListe} = await appelApi();
 
     const workGallery = document.querySelector(".gallery");
-    workGallery.innerHTML = ""; // on vide la galerie avant de l'afficher à nouveau
+    workGallery.innerHTML = ""; // On vide la galerie avant de l'afficher à nouveau
 
+    // Création du filtre "Tous" :
     const filtreTous = document.createElement("button");
     filtreTous.innerText = "Tous";
-    filtreTous.addEventListener("click", () => affichageWorks(worksListe));
+    filtreTous.addEventListener("click", () => {
+        const allBtn = document.querySelectorAll(".categories-filters button")
+        allBtn.forEach(btn => btn.classList.remove("selected")); 
+        filtreTous.classList.add("selected"); // Le bouton "Tous" devient vert au clique.
+        affichageWorks(worksListe)
+    });
     const categoriesFiltres = document.querySelector(".categories-filters");
 
     categoriesFiltres.appendChild(filtreTous);
-        
+    
+    // Création des autres filtres :
     for(let i = 0; i < filtresListe.length; i++) {
         const filtre = filtresListe[i];
 
@@ -57,12 +68,20 @@ async function affichageFiltres() {
         btnFiltres.addEventListener("click", () => {
             const worksFiltres = worksListe.filter(work => work.category.id === filtre.id);
             affichageWorks(worksFiltres);
+
+            const allBtn = document.querySelectorAll(".categories-filters button");
+            allBtn.forEach(btn => btn.classList.remove("selected"));
+            
+            btnFiltres.classList.add("selected"); // Les boutons devienent verts au clique.
         });
 
         categoriesFiltres.appendChild(btnFiltres);
     }  
 
+    // Appel à la fonction pour l'affichage de base des works sans filtre :
     affichageWorks(worksListe);
 }
 
+
+// Appel à la fonction pour l'affichage de la galerie au chargement de la page : 
 affichageFiltres();
